c347378bca87ccf09fc66fd1388b6a35e9399985
Faith Chepngeno <faithchepngeno850@gmail.com>
2026-01-24 17:27:49 +0300

    Created PostDetailView and its corresponding url

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CreatePost, PostList
+from .views import CreatePost, PostList, PostDetail
 
 urlpatterns = [
     path('',PostList.as_view(), name='post_list'),
     path('create/',CreatePost.as_view(), name='create_post'),
+    path('post/<int:pk>/',PostDetail.as_view(), name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -19,7 +19,12 @@ class PostList(ListView):
     template_name='blog/post_list.html'
     context_object_name = 'posts'
 
-    
+    def get_queryset(self):
+        return super().get_queryset().order_by('-created_at')
 
 
+class PostDetail(DetailView):
+    model=Post
+    template_name='blog/post_detail.html'
+    context_object_name = 'post'
 
