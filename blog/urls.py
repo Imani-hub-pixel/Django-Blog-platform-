727f1e918e973db403c1e8a7a451b5d4f02ab039
Faith Chepngeno <faithchepngeno850@gmail.com>
2026-01-25 15:50:53 +0300

    Created the CRUD features for comments, its urls and its corresponding templates

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import CreatePost, PostList, PostDetail,UpdatePost,DeletePost
+from .views import CreatePost, PostList, PostDetail,UpdatePost,DeletePost, CreateComment, CommentDelete, UpdateComment
 
 urlpatterns = [
     path('',PostList.as_view(), name='post_list'),
@@ -7,4 +7,7 @@ urlpatterns = [
     path('post/<int:pk>/',PostDetail.as_view(), name='post_detail'),
     path('post/<int:pk>/edit/',UpdatePost.as_view(), name='update_post'),
     path('post/<int:pk>/delete/',DeletePost.as_view(), name='delete_post'),
+    path('post/<int:pk>/comment/',CreateComment.as_view(), name='create_comment'),
+    path('comment/<int:pk>/edit/',UpdateComment.as_view(), name='update_comment'),
+    path('comment/<int:pk>/delete/',CommentDelete.as_view(), name='delete_comment'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
+from multiprocessing import context
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Post, Comment
-
+from .forms import CommentForm
 # Create your views here.
 class CreatePost(CreateView):
     model=Post 
@@ -28,6 +29,11 @@ class PostDetail(DetailView):
     template_name='blog/post_detail.html'
     context_object_name = 'post'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['comment_form'] = CommentForm()
+        return context
+    
 class UpdatePost(UpdateView):
     model=Post
     fields='__all__'
@@ -38,3 +44,29 @@ class DeletePost(DeleteView):
     model=Post
     template_name='blog/delete_post.html'
     success_url=reverse_lazy('post_list')
+
+class CreateComment(CreateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = "blog/comment_form.html"
+    success_url = reverse_lazy('post_list')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+
+    
+class UpdateComment(UpdateView):
+    model=Comment
+    fields='__all__'
+    template_name='blog/comment_form.html'
+    success_url=reverse_lazy('post_detail')
+
+
+class CommentDelete(DeleteView):
+    model=Comment
+    template_name='blog/delete_comment.html'
+    success_url=reverse_lazy('post_detail')
+
+    
